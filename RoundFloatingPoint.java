bd65f37e06c714d082430b550623558458d58746
A0Y68 <120485162+A0Y68@users.noreply.github.com>
2023-11-30 21:22:48 +0530

    Update RoundFloatingPoint.java

--- RoundFloatingPoint.java
+++ RoundFloatingPoint.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 public class RoundFloatingPoint {   
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a no");
+        
+        System.out.print("\nEnter a Floating Point Number:");
         double n = sc.nextDouble();
-        System.out.println("Enter no of decimal places");
+        
+        System.out.print("\nEnter Number of Decimal Places to be Rounded Off: ");
         int c = sc.nextInt();
-        n = Math.round(n * Math.pow(10, c)) / Math.pow(10, c);
-        System.out.println(n);
+        
+        double round = Math.round(n * Math.pow(10, c)) / Math.pow(10, c);
+        System.out.print("\nRounded off Number is: " + round);
+        
         sc.close();
     }
 }
