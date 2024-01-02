0a2a0d5e8133de234010014f83186d95d6def0f1
Arpan <120485162+A0Y68@users.noreply.github.com>
2024-01-02 17:59:59 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -8,17 +8,18 @@ public class pattern {
         int h = sc.nextInt();
         System.out.println();
         for(int i = 1;i<=h;i++){
-
-            for(int sp = 1; sp <= h-i; sp++)
-                System.out.print(" ");
-            
-            for(int j = i; j>=1;j--)
-                System.out.print(j);
-            
-            for(int j = 2; j<=i;j++)
-                System.out.print(j);
-            
-            System.out.println();
+            if(i!=h/2+1){
+                for(int sp = 1; sp <= h-i; sp++)
+                    System.out.print(" ");
+                
+                for(int j = i; j>=1;j--)
+                    System.out.print(j);
+                
+                for(int j = 2; j<=i;j++)
+                    System.out.print(j);
+                
+                System.out.println();
+            }
         }
         sc.close();
     }
