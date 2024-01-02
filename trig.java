05bd37797588f433b88b3a4a1ffa5b0fbed47b90
Arpan <120485162+A0Y68@users.noreply.github.com>
2024-01-02 17:58:47 +0530

    Update trig.java

--- trig.java
+++ trig.java
@@ -4,27 +4,41 @@ import java.text.DecimalFormat;
 public class trig {
     public static void main(String[] args) {
 
-            DecimalFormat df = new DecimalFormat("0.00");
+        DecimalFormat df = new DecimalFormat("0.00");
 
-            System.out.println("\nTrig\t0\t30\t45\t60\t90\n");
+        System.out.println("\nTrig\t0\t30\t45\t60\t90\n");
 
-            for(int i = 1; i<=3; i++){
+        int i=1;
+
+        do{
 
             if(i==1) System.out.print("Sin\t");
             else if(i==2) System.out.print("Cos\t");
             else System.out.print("Tan\t");
 
-            for(int theta = 0; theta<=90; theta+=15){
-                if(theta == 15 || theta == 75) continue;
-                else{
-                double angle = Math.toRadians(theta);
+            int theta=0;
 
-                if(i==1) System.out.print(df.format(Math.sin(angle))+"\t");
-                else if(i==2) System.out.print(df.format(Math.cos(angle))+"\t");
-                else System.out.print(df.format(Math.tan(angle))+"\t");
+            do{
+                if(theta != 15 && theta != 75){
+                    double angle = Math.toRadians(theta);
+
+                    if(i==1) 
+                        System.out.print(df.format(Math.sin(angle))+"\t");
+
+                    else if(i==2) 
+                        System.out.print(df.format(Math.cos(angle))+"\t");
+
+                    else{
+                        if(theta!=90) 
+                            System.out.print(df.format(Math.tan(angle))+"\t");
+                        else    
+                            System.err.print("Undefined");
+                    }
                 }
-            }
+                theta+=15;
+            }while(theta<=90);
             System.out.println();
-        }
+            i++;
+        }while(i<=3);
     }
 }
